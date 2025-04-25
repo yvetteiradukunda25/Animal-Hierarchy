a864bafdf11d3f9d7e70ab77ee440a9112a5e4fc
Yvette IRADUKUNDA <iradyvette98@gmail.com>
2025-04-25 20:27:24 +0200

    Adding changes

--- src/Animal.java
+++ src/Animal.java
@@ -1,4 +1,48 @@
-package PACKAGE_NAME;
-
 public class Animal {
+    String name;
+    public Animal(String name) {
+        this.name = "Animal";
+    }
+    public void makeSound() {
+        System.out.println("Animal sounds:");
+    }
+}
+
+//Dog
+
+class Dog extends Animal {
+    public Dog(String name) {
+        super(name);
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Woof!");
+    }
+}
+
+//Cat
+
+class Cat extends Animal {
+    public Cat(String name) {
+        super(name);
+    }
+    @Override
+    public void makeSound() {
+        System.out.println("Meow!");
+    }
+}
+// Bird
+class Bird extends Animal {
+    public Bird( String name){
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Chirp!");
+    }
+
+    public void fly() {
+        System.out.println("Flying!");
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Dog dog = new Dog("Max");
+        Cat cat = new Cat("Bubbles");
+        Bird bird = new Bird("Blue Jay");
+
+        // Dog
+        System.out.print("Dog says: ");
+        dog.makeSound();//Outputs the sound that the dog makes.
+
+        // Cat
+        System.out.print("Cat says: ");
+        cat.makeSound();//Outputs the sound that the cat makes.
+
+        // Bird
+        System.out.print("Bird says: ");
+        bird.makeSound();//Outputs the sound that the bird makes.
+
+        System.out.print("Bird does: ");
+        bird.fly();//Outputs the sound that the bird does(bird flies).
+
+        Animal birdPoly = new Bird("Polly");
+        System.out.println("Polymorphic Bird says:");
+        birdPoly.makeSound();
     }
 }
